7d59d9ab51184fa56f71652d6119d48909c08962
Xander Jones <xander@bugsnag.com>
2021-08-17 18:36:08 +0100

    fixed bug causing overpopulation

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,17 @@ Bugsnag.start({
 })
 
 var autoRunTimeout: ReturnType<typeof setTimeout>;
+const mapSizeDefault = {
+    rows: 40,
+    columns: 50
+}
+
+const glider: number[][] = [
+    [2,2],
+    [2,3],
+    [2,4]
+]
+
 
 function pageBootstrap(): void {
     document.getElementById("version").innerText = `v${packageInfo.version}`;
@@ -26,7 +37,7 @@ function pageBootstrap(): void {
         gol.nextGeneration();
     }
     document.getElementById("resetBtn").onclick = function() {
-        gol = new GameOfLife(50, 40);
+        gol = new GameOfLife(mapSizeDefault.rows, mapSizeDefault.columns);
     }
 
     let autoNextGeneration = document.getElementById("autoNextGeneration") as HTMLInputElement;
@@ -45,44 +56,67 @@ class GameOfLife {
     grid: boolean[][];
     generation: number;
     cellsAlive: number;
-    sizex: number;
-    sizey: number;
+    rows: number;
+    columns: number;
 
-    constructor(sizex: number, sizey: number) {
-        this.sizex = sizex;
-        this.sizey = sizey;
+    constructor(rows: number, columns: number, pattern: number[][] = null) {
+        this.rows = rows;
+        this.columns = columns;
         this.generation = 0;
         this.cellsAlive = 0;
-        this.grid = 
-            Array.from({ length: sizex }, () => (
-                Array.from({ length: sizey }, () => (
-                    Math.random() < 0.1
-                ))
+        
+        if (!pattern) {
+            this.grid = 
+                Array.from({ length: rows }, () => (
+                    Array.from({ length: columns }, () => (
+                        Math.random() < 0.1
+                    ))
             ))
+        } else {
+            this.grid = Array.from({ length: rows }, () => 
+                Array.from({ length: columns }, () => false)
+            );
+            pattern.forEach((el) => {
+                this.grid[el[0]][el[1]] = true;
+            });
+        }
         this.pushGridToDOM();
     }
 
+    debugObj = {
+        start_Live: false,
+        start_Neighbours: -1,
+        end___Live: false,
+    }
+
     nextGeneration(autoRun: boolean = false): void {
-        var newGrid: boolean[][] = this.grid.slice();
-        this.grid.forEach((xel, x) => {
-            xel.forEach((yel, y) => {
-                newGrid[x][y] = this.calcLiveOrDie(this.grid[x][y], this.getNeighbourStates(x, y))
+        var newGrid: boolean[][] = Array.from({ length: this.rows }, () => 
+            Array.from({ length: this.columns }, () => false)
+        );
+        this.grid.forEach((row, row_index) => {
+            row.forEach((_, col_index) => {
+                newGrid[row_index][col_index] = this.calcLiveOrDie(row_index, col_index)
+                this.debugObj.start_Live = this.grid[row_index][col_index];
+                this.debugObj.end___Live = newGrid[row_index][col_index];
             });
         });
         this.grid = newGrid;
+        this.generation += 1;
         this.pushGridToDOM();
         if (autoRun) {
             autoRunTimeout = setTimeout(function() { gol.nextGeneration(true) }, 10)
         }
     }
 
-    calcLiveOrDie(cell: boolean, neighbours: boolean[]): boolean {
+    calcLiveOrDie(row: number, column: number): boolean {
+        let neighbours: boolean[] = this.getNeighbourStates(row, column);
         let liveNeighbours: number = 0;
         neighbours.forEach((neighbour: boolean) => {
             liveNeighbours += (neighbour) ? 1 : 0;
         })
+        this.debugObj.start_Neighbours = liveNeighbours;
 
-        if (cell) { 
+        if (this.grid[row][column]) { 
             // cell is alive
             if (liveNeighbours >= 2 && liveNeighbours <= 3) {
                 return true;
@@ -99,28 +133,28 @@ class GameOfLife {
         }
     }
 
-    getNeighbourStates(x: number, y: number): boolean[] {
+    getNeighbourStates(row: number, column: number): boolean[] {
         let neighbours: boolean[] = [];
         let directions: number[][] = [
-            [0,-1], // N
-            [1,-1], // NE
-            [1,0], // E
+            [-1,0], // N
+            [-1,1], // NE
+            [0,1], // E
             [1,1], // SE
-            [0,1], // S
-            [-1,1], // SW
-            [-1,0], // W
-            [-1,-1], // NW
+            [1,0], // S
+            [1,-1], // SW
+            [0,-1], // W
+            [-1,-1] // NW
         ]
         directions.forEach((direction) => {
-            let newX = x+direction[0]
-            let newY = y+direction[1]
-            if (newX < 0 ||
-                newY < 0 ||
-                newX > this.sizex-1 ||
-                newY > this.sizey-1) {
+            let new_row = row+direction[0]
+            let new_column = column+direction[1]
+            if (new_row < 0 ||
+                new_column < 0 ||
+                new_row > this.rows-1 ||
+                new_column > this.columns-1) {
                 neighbours.push(false);
             } else {
-                neighbours.push(this.grid[newX][newY]);
+                neighbours.push(this.grid[new_row][new_column]);
             }
         });
         return neighbours;
@@ -131,27 +165,28 @@ class GameOfLife {
         newTable.id = "tableGol";
         this.cellsAlive = 0;
 
-        for (var y = 0; y < this.sizey; y++) {
-            let row = newTable.insertRow();
-            for (var x = 0; x < this.sizex; x++) {
-                let cell = row.insertCell();
-                if (this.grid[x][y] === true) {
+        for (var row = 0; row < this.rows; row++) {
+            let tr = newTable.insertRow();
+            for (var column = 0; column < this.columns; column++) {
+                let cell = tr.insertCell();
+                if (this.grid[row][column] === true) {
                     cell.appendChild(document.createTextNode("■"));
-                    this.cellsAlive += 1;   
+                    this.cellsAlive += 1;
                 } else {
-                    cell.appendChild(document.createTextNode(" "));
+                    cell.appendChild(document.createTextNode("_"));
                 }
             }
         }
         let table = document.getElementById("tableGol");
         if (table == null) {
-            document.getElementById("gol").appendChild(newTable);
+            document.getElementById("golDiv").appendChild(newTable);
         } else {
             table.replaceWith(newTable);
         }
         document.getElementById("cellsAliveCount").innerText = String(this.cellsAlive);
+        document.getElementById("generation").innerText = String(this.generation);
     }
 }
 
 pageBootstrap();
-let gol = new GameOfLife(50, 40);
+let gol = new GameOfLife(mapSizeDefault.rows, mapSizeDefault.columns, glider);
